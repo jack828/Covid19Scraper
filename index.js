a45f16a2498f7fdcf3d626803dd50f55c9d373be
Jack Burgess <jackburgess124@gmail.com>
2020-03-12 18:15:23 +0000

    refactor out screenshot code

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ const promisify = require('util').promisify
 const token = process.env.SLACK_TOKEN
 const channel = process.env.SLACK_CHANNEL
 
-;(async () => {
+const takeScreenshot = async ({
+  filename,
+  url,
+  elementsToRemove,
+  leftHandElementId,
+  rightHandElementIds
+}) => {
+  console.log(`Screenshotting ${filename}`)
+
   const browser = await puppeteer.launch({ headless: true })
   const page = await browser.newPage()
 
@@ -20,72 +28,79 @@ const channel = process.env.SLACK_CHANNEL
     height: 1080
   })
 
-  await page.goto(
-    'https://www.arcgis.com/apps/opsdashboard/index.html#/f94c3c90da5b4e9f9a0b19484dd4bb14',
-    { waitUntil: 'networkidle0' }
-  )
+  await page.goto(url, { waitUntil: 'networkidle0' })
 
-  // await page.waitFor(2000)
   console.log('Navigation complete')
 
-  await page.evaluate(sel => {
-    var elements = document.querySelectorAll(sel)
+  await page.evaluate(elements => {
     for (var i = 0; i < elements.length; i++) {
-      const el = elements[i]
-      el.parentElement.parentElement.removeChild(el.parentElement)
+      const el = document.getElementById(elements[i])
+      el.parentElement.removeChild(el)
     }
-  }, '.map-container')
+  }, elementsToRemove)
 
-  await page.evaluate(() => {
-    var cases = document.getElementById('ember77')
+  await page.evaluate((leftHandEl, rightHandEls) => {
+    var cases = document.getElementById(leftHandEl)
     var width = cases.style.width
 
-    var caseTableElementIds = [
+    rightHandEls.forEach(id => {
+      var el = document.getElementById(id)
+      el.style.left = width
+    })
+  }, leftHandElementId, rightHandElementIds)
+
+  const screenshot = await page.screenshot({
+    path: filename,
+    clip: { x: 0, y: 0, width: 1200, height: 1080 }
+  })
+  await browser.close()
+  console.log(`Screenshotted ${filename}`)
+}
+
+const uploadScreenshot = async ({ filename }) => {
+  console.log(`Uploading ${filename}`)
+  const res = await promisify(request)({
+    url: `https://slack.com/api/files.upload`,
+    method: 'POST',
+    formData: {
+      token,
+      channels: channel,
+      title: `${moment().format('YYYY-MM-DD')}-${filename}`,
+      filename,
+      filetype: 'image/png',
+      file: fs.createReadStream('infect.png')
+    }
+  })
+  console.log(`Uploaded ${filename}`, res.statusCode, res.body)
+}
+
+;(async () => {
+  await takeScreenshot({
+    filename: 'uk-cases.png',
+    url:
+      'https://www.arcgis.com/apps/opsdashboard/index.html#/f94c3c90da5b4e9f9a0b19484dd4bb14',
+    elementsToRemove: ['ember85'],
+    leftHandElementId: 'ember77',
+    rightHandElementIds: [
       'ember90',
       'ember95',
       'ember102',
       'ember109',
       'ember120'
     ]
-
-    caseTableElementIds.forEach(id => {
-      var el = document.getElementById(id)
-      el.style.left = width
-    })
   })
-  // await page.goto(
-  // 'https://www.charles-stanley-direct.co.uk/My_Dashboard/My_Direct_Accounts/Combined_Portfolio'
-  // )
 
-  console.log('Screenshotting')
-  const screenshot = await page.screenshot({
-    path: 'infect.png',
-    clip: { x: 0, y: 0, width: 1200, height: 1080 }
-  })
-  await browser.close()
-  console.log('Done.')
-  console.log('Uploading')
+  await uploadScreenshot({ filename: 'uk-cases.png' })
 
-  let res = await promisify(request)({
-    url: `https://slack.com/api/files.upload`,
-    method: 'POST',
-    formData: {
-      token,
-      channels: channel,
-      title: moment().format('YYYY-MM-DD'),
-      filename: 'image.png',
-      filetype: 'image/png',
-      file: fs.createReadStream('infect.png')
-    }
-  })
-  console.log(res.statusCode, res.body)
 
+  // process.exit(0)
   res = await promisify(request)({
     url: `https://slack.com/api/chat.postMessage?${qs.stringify({
       token,
       channel,
       as_user: true,
-      text: 'How to get through it:\n1. Don’t panic!\n2. Wash hands\n3. Drink tea\n4. ???\n5. Profit!'
+      text:
+        'How to get through it:\n1. Don’t panic!\n2. Wash hands\n3. Drink tea\n4. ???\n5. Profit!'
     })}`,
     method: 'POST'
   })

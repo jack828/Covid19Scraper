754b06aa10cebcae7ae29ed4558423827b8b5c25
Jack Burgess <jackburgess124@gmail.com>
2020-03-13 00:39:46 +0000

    extract uk data & refactor

--- datascraper.js
+++ datascraper.js
@@ -3,10 +3,12 @@ const puppeteer = require('puppeteer')
 const moment = require('moment')
 const fs = require('fs')
 
-const scrapeResultsTable = async (browser, { url, filename, extract }) => {
+const scrapeResultsTable = async (
+  browser,
+  { url, filename, tableContainer, extractRegex }
+) => {
   console.log(`Scraping ${filename}`)
   const page = await browser.newPage()
-  let results = ''
 
   console.log('Launched Puppeteer')
   await page.setViewport({
@@ -22,7 +24,28 @@ const scrapeResultsTable = async (browser, { url, filename, extract }) => {
 
   console.log('Navigated')
 
-  results = await page.evaluate(...extract)
+  const results = await page.evaluate(
+    (tableContainer, extractRegex) => {
+      const container = document.getElementById(tableContainer)
+      const table = container.querySelector('nav.feature-list')
+      const rows = table.querySelectorAll('span.feature-list-item')
+
+      const rawData = [...rows].map(row => row.innerText)
+      const data = [...new Set(rawData)]
+      // console.log(data.toString())
+
+      const formatted = data.reduce((world, country) => {
+        const regex = new RegExp(extractRegex)
+        const [, countryName, caseCount] = regex.test(country)
+          ? country.match(regex)
+          : []
+        return { ...world, [countryName.trim()]: caseCount }
+      }, {})
+      return formatted
+    },
+    tableContainer,
+    extractRegex
+  )
 
   await page.close()
   console.log(`Scraped ${filename}`)
@@ -35,31 +58,28 @@ const calcTotal = data =>
 ;(async () => {
   const browser = await puppeteer.launch({ headless: true, devtools: true })
 
+  const ukResults = await scrapeResultsTable(browser, {
+    url:
+      'https://www.arcgis.com/apps/opsdashboard/index.html#/f94c3c90da5b4e9f9a0b19484dd4bb14',
+    filename: 'uk-cases',
+    tableContainer: 'ember95',
+    extractRegex: '(.*?):.?(\\d*)'
+  })
+
+  const ukCases = {
+    date: moment().format('YYYY-MM-DD'),
+    total: calcTotal(ukResults),
+    data: ukResults
+  }
+  fs.writeFileSync('uk-cases.json', JSON.stringify(ukCases, null, 2))
+  // console.log(ukResults)
+
   const globalResults = await scrapeResultsTable(browser, {
     url:
       'https://who.maps.arcgis.com/apps/opsdashboard/index.html#/bf48be9799364068be4706c56b1916f5',
     filename: 'global-cases',
-    extract: [
-      tableContainer => {
-        const container = document.getElementById(tableContainer)
-        const table = container.querySelector('nav.feature-list')
-        const rows = table.querySelectorAll('span')
-
-        const rawData = [...rows].map(row => row.innerText)
-        const data = [...new Set(rawData)]
-        console.log(data.toString())
-
-        const formatted = data.reduce((world, country) => {
-          const regex = /(.*?):(\d*)/ // NO TESTS WHEEEEEE
-          const [, countryName, caseCount] = regex.test(country)
-            ? country.match(regex)
-            : []
-          return { ...world, [countryName.trim()]: caseCount }
-        }, {})
-        return formatted
-      },
-      'ember80'
-    ]
+    tableContainer: 'ember80',
+    extractRegex: '(.*?):(\\d*)'
   })
 
   const globalCases = {
@@ -68,7 +88,8 @@ const calcTotal = data =>
     data: globalResults
   }
   fs.writeFileSync('global-cases.json', JSON.stringify(globalCases, null, 2))
-  console.log(globalResults)
+  // console.log(globalResults)
+  //
   await browser.close()
   process.exit(0)
 })()

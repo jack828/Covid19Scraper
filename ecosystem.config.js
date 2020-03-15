0d04135d1744fcd2084df3f10b959f8646f31ce0
Jack Burgess <jackburgess124@gmail.com>
2020-03-15 17:04:06 +0000

    add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: 'COVID',
+      script: 'server.js',
+
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 5000
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 5000
+      }
+    }
+  ]
+}

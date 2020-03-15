9fb8747b060a46e4630b6771eabf95bd376da25c
Jack Burgess <jackburgess124@gmail.com>
2020-03-15 16:23:22 +0000

    slack interactivity endpoints

--- server.js
+++ server.js
@@ -3,14 +3,24 @@ const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const port = process.env.PORT || 5000
+const createSlackRoutes = require('./lib/slack/')
+const fs = require('fs')
+
+const readData = () => {
+  const globalData = JSON.parse(fs.readFileSync('./global-cases.json'))
+  const ukData = JSON.parse(fs.readFileSync('./uk-cases.json'))
+  const allData = {
+    'filter-country': globalData,
+    'filter-county': ukData
+  }
+  return allData
+}
 
 app.use(morgan('dev'))
 
-app.get('/api', (req, res) => {
-  res.sendStatus(418)
-})
+createSlackRoutes(app, { readData })
 
-app.listen(port, (error) => {
+app.listen(port, error => {
   if (error) throw error
   console.log(`Listening ${port}`)
 })

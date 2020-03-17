a5353d14bf4a6e5e29c5b843e1434261ac19ae28
Jack Burgess <jackburgess124@gmail.com>
2020-03-17 21:07:09 +0000

    add fortune command (knew it would come in handy)

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const morgan = require('morgan')
 const port = process.env.PORT || 5000
 const createSlackRoutes = require('./lib/slack/')
 const fs = require('fs')
+const { exec } = require('child_process')
 
 const readData = () => {
   const globalData = JSON.parse(fs.readFileSync('./global-cases.json'))
@@ -17,9 +18,17 @@ const readData = () => {
   return allData
 }
 
+const execute = command =>
+  new Promise(resolve =>
+    exec(command, (error, stdout) => {
+      if (error) return reject(error)
+      resolve(stdout)
+    })
+  )
+
 app.use(morgan('dev'))
 
-createSlackRoutes(app, { readData })
+createSlackRoutes(app, { readData, execute })
 
 app.listen(port, error => {
   if (error) throw error
